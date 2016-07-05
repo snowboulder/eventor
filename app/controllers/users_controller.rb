bb5b21264c4127bd1fcee33fd6c86a73b28483f3
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-07-05 16:05:22 -0400

    Add upcoming and past events method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = @user.created_events
+    @upcoming_events = @user.attended_events.upcoming
+    @prev_events = @user.attended_events.past
   end
 
   def index
     @user = User.all
   end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,16 @@
 class Event < ActiveRecord::Base
+
+  scope :upcoming, -> { where(:date >= :presentTime, presentTime: Time.now.to_date ) }
+  scope :past,     -> { where(:date <= :presentTime, presentTime: Time.now.to_date ) }
+
   belongs_to :creator, :class_name => "User"
 
   has_many :invitations, :foreign_key => "attended_event_id"
   has_many :attendees, through: :invitations
 
-  validates :title, presence: true
+  validates :title,       presence: true
   validates :description, presence: true, length: { maximum: 500 }
-  validates :location, presence: true
+  validates :location,    presence: true
+  validetes :date,        presence: true
 
 end
